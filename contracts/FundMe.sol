7a87c548ebfa4d91c0d2de6cac8570149ddc68a7
IrmawanAriel <arielirmawan@gmail.com>
2025-05-18 18:39:11 +0700

    Payable and receive, encounter new bug on priceConverting.sol

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.2 <0.9.0;
 
-import {PriceConverter} from "./priceConverter.sol";
+// import {PriceConverter} from "./priceConverter.sol";
 
 contract FundMe {
 
-    using PriceConverter for uint256; // assign library for all uint256 type data
+    // using PriceConverter for uint256; // assign library for all uint256 type data
 
-    uint256 public constant MinimumUSD = 5e18;
+    uint256 public constant MinimumUSD = 5;
 
     address public immutable owner;
 
@@ -19,10 +19,10 @@ contract FundMe {
     mapping (address funder => uint256 amountFunded) public addressToAmountFunded;
 
     function fund () public payable {
-        // require(msg.value.getConventionalRate() >= MinimumUSD, "didn't send enough eth"); // reverts
-        if(msg.value.getConventionalRate() >= MinimumUSD) {
-            revert();
-        }
+        require(msg.value >= MinimumUSD, "didn't send enough eth"); // reverts
+        // if(msg.value.getConventionalRate() <= MinimumUSD) {
+        //    revert();
+        // }
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value ;
     }
@@ -39,22 +39,30 @@ contract FundMe {
         // bool sendSuccess = payable(msg.sender).send(address(this).balance); // send method
         // require(sendSuccess, "send failed");
 
-        // this is the bestpractice
+        // this is the bestpractice to transfer
         (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}(""); // call method, with destructuring
-        // require(callSuccess, "call failed");
-        if(callSuccess){
-            revert();
-        }
+         require(callSuccess, "call failed");
+        //if(!callSuccess){
+          //  revert();
+        //}
 
     }
 
     modifier onlyOwner () {
-        // require(msg.sender == owner, "you're not the owner" );
-        if(msg.sender == owner){
-            revert();
-        }
+        require(msg.sender == owner, "you're not the owner" );
+        //if(msg.sender != owner){
+        //    revert();
+        //}
         _;
     }
+
+    receive() external payable { // if theres no data send in transaction. default processing will go into fund function
+        fund();
+    }
+
+    fallback() external payable {// if theres a data sended in transaction. defaul processing set to go to fund function
+        fund();
+     }
 
 
 }
